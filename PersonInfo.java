8b973785b40579345428cd5f267b9514126960a1
jameliah45-code <jameliah45@gmail.com>
2025-11-17 22:32:47 -0500

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,17 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        String firstName = "Jordan";
+        String lastName = "Smith";
+        String streetAddress = "123 Maple Avenue";
+        String city = "Springfield";
+        String zipCode = "54321";
+
+        // Print each variable on its own line
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
